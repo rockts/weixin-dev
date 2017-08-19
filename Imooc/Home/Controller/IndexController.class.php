aa8d1dc27c2c2979e5c1e1c09e659496921c07ae
gaopeng <service@lekee.cc>
2017-08-20 03:36:30 +0800

    加入自定义菜单

--- Imooc/Home/Controller/IndexController.class.php
+++ Imooc/Home/Controller/IndexController.class.php
@@ -76,7 +76,7 @@ class IndexController extends Controller {
      * $res 返回数据类型 string
      * $arr post请求参数 string
      */
-    function http_curl($url, $type='get', $res='json'){
+    function http_curl($url, $type='get', $res='json', $arr=''){
       //1.初始化curl
       $ch = curl_init();
       //2.设置curl的参数
@@ -91,35 +91,42 @@ class IndexController extends Controller {
       //4.关闭
       curl_close($ch);
       if($res=='json'){
-        return json_decode($output, true);
+        if( curl_errno($ch) ){
+          //请求失败，返回错误信息
+          return curl_error($ch);
+        }else{
+          //请求成功
+          return json_decode($output, true);
+        }
       }
     }
 
-     function getWxAccessToken(){
-       //1.请求url地址
-       $appid = 'wx29ce29e1eb671505';
-       $appsecret = '21f2683c879a555c1503e822f47a2a9d';
-       $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=$appid&secret=$appsecret";
-       //2.初始化
-       $ch = curl_init();
-       //3.设置参数
-       curl_setopt($ch, CURLOPT_URL, $url);
-       curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-       //4.调用接口
-       $res = curl_exec($ch);
-       //5.关闭url
-       curl_close( $ch );
-       if( curl_error($ch) ){
-         var_dump( curl_error($ch) );
-       }
-       $arr = json_decode($res, true);
-       var_dump( $arr );
-     }
+    //  function getWxAccessToken(){
+    //    //1.请求url地址
+    //    $appid = 'wx29ce29e1eb671505';
+    //    $appsecret = '21f2683c879a555c1503e822f47a2a9d';
+    //    $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".$appid."&secret=".$appsecret;
+    //    //2.初始化
+    //    $ch = curl_init();
+    //    //3.设置参数
+    //    curl_setopt($ch, CURLOPT_URL, $url);
+    //    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    //    //4.调用接口
+    //    $res = curl_exec($ch);
+    //    //5.关闭url
+    //    curl_close( $ch );
+    //    if( curl_error($ch) ){
+    //      var_dump( curl_error($ch) );
+    //    }
+    //    $arr = json_decode($res, true);
+    //    var_dump( $arr );
+    //  }
 
      function getWxServerIp(){
-       $accessToken = "_X25puqe-u3e-ikx9AeMjnZ5MlSAEetIxrR10sCodIJkQh-5zLPDH2a4kEQsvGGl6G4_lDdPd6KSCSoHUZ-3lDav1R18Wyy8SguUBmcUaPw96NB6lBsJ4AxlVAYEMjE7EBIaABAIFM";
+       $accessToken = "DaSKgL9TF3Byq0uthDr3GafHxc94KAdl-Mkb2fFH09g71hcdzU-ieW8JJzz_jdDz_mQM9jACvO6mAv0fXho3ZK4ChCeX-AvbhjilBq67p_IHPVaAAASAG";
        $url = "https://api.weixin.qq.com/cgi-bin/getcallbackip?access_token=".$accessToken;
        $ch = curl_init();
+       curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
        curl_setopt($ch, CURLOPT_URL, $url);
        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
        $res = curl_exec($ch);
@@ -133,18 +140,68 @@ class IndexController extends Controller {
        echo "</pre>";
      }
 
+     //返回access_token *session解决方法 存mysql memcache
+     public function getWxAccessToken(){
+       //将access_token 存在seession/cookie中
+       if( $_SESSION['access_token'] && $_SESSION['expire_time']>time()){
+         //如果access_token在session并没有过期
+         return $_SESSION['access_token'];
+       }else{
+         //如果access_token不存在或者已经过期，重新取access_token
+         $appid = 'wx29ce29e1eb671505';
+         $appsecret = '21f2683c879a555c1503e822f47a2a9d';
+         $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".$appid."&secret=".$appsecret;
+         $res = $this->http_curl($url, 'get', 'json');
+         $access_token = $res['access_token'];
+         //将重新获取到的access_token存到session
+         $_SESSION['access_token'] = $access_token;
+         $_SESSION['expire_time'] = time()+7000;
+
+         return $access_token;
+       }
+     }
+
      public function definedItem(){
        //创建微信菜单
        //目前微信接口的调用方式都是通过curl post/get
-       $access_token = "";
+       echo $access_token = $this->getWxAccessToken();
+       echo '<br />';
        $url = " https://api.weixin.qq.com/cgi-bin/menu/create?access_token=".$access_token;
        $postArr = array(
-         array(), //第一个一级菜单
-         array(), //第二个一级菜单
-         array(), //第三个一级菜单
+         'button'=>array(
+           array(
+             'name'=>urlencode('菜单一'),
+             'type'=>'click',
+             'key'=>'item ',
+           ), //第一个一级菜单
+           array(
+             'name'=>urlencode('菜单二'),
+             'sub_button'=>array(
+               array(
+                 'name'=>urlencode('歌曲'),
+                 'type'=>'click',
+                 'key'=>'songs',
+               ),//第一个二级菜单
+               array(
+                 'name'=>urlencode('电影'),
+                 'type'=>'view',
+                 'url'=>'http://www.baidu.com',
+               ),//第二个二级菜单
+             ),
+           ), //第二个一级菜单
+           array(
+             'name'=>urlencode('菜单三'),
+             'type'=>'view',
+             'url'=>'http://www.qq.com'
+           )//第三个一级菜单
+         ),
        );
-       $postJson = json_encode( $postArr );
+       echo '<hr />';
+       var_dump($postArr);
+       echo '<hr />';
+       echo $postJson = urldecode( json_encode( $postArr ) );
        $res = $this->http_curl($url, 'post', 'json', $postJson);
+       echo "<hr />";
        var_dump($res);
      }
 }//class end
